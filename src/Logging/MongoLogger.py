fcb6d924519d0b51904dd44b3dedbba1328d2c01
Geeoon Chung <geeoon13@gmail.com>
2026-01-27 11:39:31 -0800

    Added timestamp to MongoDB logging

--- src/Logging/MongoLogger.py
+++ src/Logging/MongoLogger.py
@@ -3,6 +3,7 @@ MongoLogger derived class from Logger
 TODO: discuss to async or not
 """
 
+import time
 from pymongo import MongoClient
 
 from .Logger import Logger, LogLevel
@@ -18,13 +19,14 @@ class MongoLogger(Logger):
         self.collection = self.db[collection]
 
     def log(self, data: dict, level: LogLevel=LogLevel.INFO) -> bool:
-        ret = self.collection.insert_one(data | { "level": level.value[1] })
+        ret = self.collection.insert_one(data | { "level": level.value[1], "time": round(time.time() * 1000) })
         return True if ret.inserted_id else False
 
     def log_many(self, data: list[dict], levels: list[LogLevel]=[]) -> bool:
         assert(len(data) == len(levels))
-        ret = self.collection.insert_many([d | { "level": l.value[1] } for d, l in zip(data, levels)])
+        ret = self.collection.insert_many([d | { "level": l.value[1], "time": round(time.time() * 1000) } for d, l in zip(data, levels)])
         return True if ret.inserted_ids else False
     
     def close_connection(self):
         self.client.close()
+
