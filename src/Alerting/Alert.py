01159c130d3962b7aefc09b432513f14386531dc
Elliot Norman <elliotjnorman@gmail.com>
2026-01-31 19:13:31 -0800

    Feat: Added alert abstract class and started emailalert system

---
+++ src/Alerting/Alert.py
@@ -0,0 +1,24 @@
+"""
+Alert base class
+"""
+
+from abc import ABC, abstractmethod
+from Logging.Logger import LogLevel
+
+class Alert(ABC):
+    """
+    Abstract class for alerting
+    """
+    @abstractmethod
+    def alert(self, data: dict, level: LogLevel) -> bool:
+        """
+        Abstract method to alert on an event
+        
+        :param data: the data to alert on
+        :type data: dict
+        :param level: the log level
+        :type level: LogLevel
+        :return: True on success, False on error
+        :rtype: bool
+        """
+        pass
---
+++ src/Alerting/EmailAlert.py
@@ -0,0 +1,39 @@
+"""
+EmailAlert derived class from Alert
+https://realpython.com/python-send-email/#make-a-csv-file-with-relevant-personal-info
+"""
+
+from .Alert import Alert
+from Logging.Logger import LogLevel
+
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+class EmailAlert(Alert):
+    """
+    EmailAlert class for alerting
+    """
+    def __init__(self):
+        pass
+
+    def alert(self, data: dict, level: LogLevel) -> bool: 
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        password = input("Type your password and press enter: ") # TODO: Use environment variables
+
+        sender_email = "my@gmail.com"
+        receiver_email = "your@gmail.com"
+        message = """\
+            Subject: Test email
+            
+            Level: {level}
+            Data: {data}
+            """
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+        return True
+
