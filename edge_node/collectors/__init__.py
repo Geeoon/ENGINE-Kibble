cd5616cd4fa975e47106be851bb2098398830efa
rohansabhaya <rohan.sabhaya34@gmail.com>
2026-04-22 17:06:39 -0700

    feat: add power data

--- edge_node/collectors/__init__.py
+++ edge_node/collectors/__init__.py
@@ -2,5 +2,4 @@ from .CpuCollector import CpuCollector
 from .MemoryCollector import MemoryCollector
 from .TemperatureCollector import TemperatureCollector
 from .DiskCollector import DiskCollector
-
-
+from .PowerCollector import PowerCollector
--- edge_node/telemetry_collector.py
+++ edge_node/telemetry_collector.py
@@ -4,13 +4,14 @@ Collects CPU usage, memory usage, and temperature on an edge node.
 
 import datetime
 import time
-from collectors import CpuCollector, MemoryCollector, TemperatureCollector
+from collectors import CpuCollector, MemoryCollector, TemperatureCollector, PowerCollector
 
 INTERVAL = 30  # seconds between collections
 
 cpu_collector = CpuCollector()
 memory_collector = MemoryCollector()
 temperature_collector = TemperatureCollector()
+power_collector = PowerCollector()
 
 records = [] # This will be changed once we have the protocol defined
 
@@ -21,5 +22,6 @@ while True:
         "cpu_percent": cpu_collector.read(),
         "memory_percent": memory_collector.read(),
         "temperature_celsius": temperature_collector.read(),
+        "power_usage_w": power_collector.read(),
     })
     time.sleep(INTERVAL)
