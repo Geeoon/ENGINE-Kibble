bedc55dd64583c07367b425291a029e13fef4032
Geeoon <geeoon13@gmail.com>
2026-04-24 12:00:14 -0700

    Added serialization

--- edge_node/KibbleDaemon/KibbleDaemon.py
+++ edge_node/KibbleDaemon/KibbleDaemon.py
@@ -2,15 +2,22 @@
 Implements the KibbleDaemon
 """
 
-from flask import Flask, Response
 
+from flask import Flask, Response
+from google.protobuf import json_format
+
+from . import protocol_pb2
 class KibbleDaemon:
     def __init__(self):
         self.app = Flask("Kibble Daemon")
         self.app.add_url_rule('/status', 'status', self._get_status, methods=['GET'])
 
     def _get_status(self) -> Response:
-        return Response('test', 200)
+        res = protocol_pb2.StatusResponse()
+        telmetry_entry = res.telemetry.add()
+        telmetry_entry.name = 'test'
+        telmetry_entry.value = 1.5
+        return Response(telmetry_entry.SerializeToString(), 200)
 
     def start(self, port: int):
         """
